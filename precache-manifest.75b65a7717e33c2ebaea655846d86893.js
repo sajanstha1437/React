3c16aa2d59a02169a13ad8bf654139815a2049dc
Sajan Shrestha <sajan.stha1437@gmail.com>
2020-12-12 20:09:52 +0900

    This is my first commit

---
+++ precache-manifest.75b65a7717e33c2ebaea655846d86893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86ca537ab6683a46a5559dcd4534b89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0a8e65909c542f27c42",
+    "url": "/static/css/main.2348b44c.chunk.css"
+  },
+  {
+    "revision": "59dc9ec4a114ae28ea10",
+    "url": "/static/js/2.f3f2545e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.f3f2545e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0a8e65909c542f27c42",
+    "url": "/static/js/main.08afc40b.chunk.js"
+  },
+  {
+    "revision": "458b4f96ab9c97c67cef",
+    "url": "/static/js/runtime-main.36994c0b.js"
+  },
+  {
+    "revision": "f6464bb98b7089501bbd504142ab8e36",
+    "url": "/static/media/corona.f6464bb9.png"
+  }
+]);
